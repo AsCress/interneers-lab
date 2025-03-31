d3b067276c553ff75e840e5c6c9683a416511ea6
Anashuman Singh <anash.cheema@gmail.com>
2025-03-31 20:40:44 +0530

    feat: added ProductCategory model

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,18 +1,29 @@
 import mongoengine as me
 from datetime import datetime
 
+
 # Create your models here.
-class Product(me.Document):
-    name = me.StringField(required=True, max_length = 255)
+class ProductCategory(me.Document):
+    title = me.StringField(required=True, unique=True, max_length=255)
     description = me.StringField()
-    category = me.StringField(required=True, max_length = 255)
+
+    meta = {"collection": "product_categories"}
+
+    def __str__(self):
+        return self.name
+
+
+class Product(me.Document):
+    name = me.StringField(required=True, max_length=255)
+    description = me.StringField()
+    category = me.ReferenceField("ProductCategory", reverse_delete_rule=me.CASCADE)
     price = me.DecimalField(required=True)
-    brand = me.StringField(required=True, max_length = 255)
-    quantity = me.IntField(default = 0, min_value=0)
+    brand = me.StringField(required=True, max_length=255)
+    quantity = me.IntField(default=0, min_value=0)
     created_at = me.DateTimeField(default=datetime.utcnow)
     updated_at = me.DateTimeField(default=datetime.utcnow)
 
-    meta = {'collection': 'products'}
+    meta = {"collection": "products"}
 
     def __str__(self):
         return self.name
--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework_mongoengine.serializers import DocumentSerializer
 from .models import Product
+from .models import ProductCategory
+
 
 class ProductSerializer(DocumentSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
+
+
+class ProductCategorySerializer(DocumentSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = "__all__"
