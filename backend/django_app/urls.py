ffd2304083b0f9c69b28c1f1102d3c85d61159d5
Anashuman Singh <anash.cheema@gmail.com>
2025-03-31 20:43:20 +0530

    feat: added CRUD APIs for ProductCategory

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
 from product.views import ProductView
+from product.views import ProductCategoryView
 from django_app.api.views import hello_name
 
 urlpatterns = [
-    path('products/', ProductView.as_view()),
-    path('products/<product_id>/', ProductView.as_view()), 
-    path('admin/', admin.site.urls),
-    path('hello/', hello_name),
+    path("products/", ProductView.as_view()),
+    path("products/<product_id>/", ProductView.as_view()),
+    path("categories/", ProductCategoryView.as_view()),
+    path("categories/<category_id>/", ProductCategoryView.as_view()),
+    path("admin/", admin.site.urls),
+    path("hello/", hello_name),
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -4,16 +4,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
+from .models import ProductCategory
 from .serializers import ProductSerializer
+from .serializers import ProductCategorySerializer
 from django.http import Http404
 from .services import ProductService
+from .services import ProductCategoryService
 from mongoengine.errors import DoesNotExist
+
 
 # Create your views here.
 class ProductPagination(PageNumberPagination):
     page_size = 5
-    page_size_query_param = 'page_size'
+    page_size_query_param = "page_size"
     max_page_size = 100
+
 
 class ProductView(APIView):
     pagination_class = ProductPagination
@@ -35,7 +40,7 @@ class ProductView(APIView):
         paginator = self.pagination_class()
         results = paginator.paginate_queryset(serializer.data, request, view=self)
         return paginator.get_paginated_response(results)
-    
+
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
@@ -55,4 +60,59 @@ class ProductView(APIView):
     def delete(self, request, product_id):
         product = self.get_product(product_id)
         ProductService.delete(product_id)
-        return Response({"message": "Product deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {"message": "Product deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+
+
+class ProductCategoryView(APIView):
+    pagination_class = ProductPagination
+
+    def get_category(self, category_id):
+        try:
+            category = ProductCategoryService.get_category(category_id)
+        except DoesNotExist:
+            raise Http404("Category does not exist")
+        return category
+
+    def get(self, request, category_id=None):
+        if category_id:
+            category = self.get_category(category_id)
+            serializer = ProductCategorySerializer(category)
+            return Response(serializer.data)
+        categories = ProductCategoryService.get()
+        serializer = ProductCategorySerializer(categories, many=True)
+        paginator = self.pagination_class()
+        results = paginator.paginate_queryset(serializer.data, request, view=self)
+        return paginator.get_paginated_response(results)
+
+    def post(self, request):
+        serializer = ProductCategorySerializer(data=request.data)
+        if serializer.is_valid():
+            category = serializer.validated_data
+            ProductCategoryService.create(category)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, category_id):
+        category = self.get_category(category_id)
+        serializer = ProductCategorySerializer(
+            category, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            category = ProductCategoryService.update(
+                category_id, serializer.validated_data
+            )
+            return Response(
+                ProductCategorySerializer(self.get_category(category_id)).data
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, category_id):
+        category = self.category(category_id)
+        ProductCategoryService.delete(category_id)
+        return Response(
+            {"message": "Category deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
